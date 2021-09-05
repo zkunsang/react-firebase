5186b9214affd91c22df978a967263fea49889fd
zkunsang <zkunsang@gmail.com>
2021-09-05 18:51:59 +0900

    first

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4CIaS7gEVDPwdWW-oFRyONGsMgaQR_DY",
+    authDomain: "react-firebase-chat-app-a733c.firebaseapp.com",
+    projectId: "react-firebase-chat-app-a733c",
+    storageBucket: "react-firebase-chat-app-a733c.appspot.com",
+    messagingSenderId: "381281768971",
+    appId: "1:381281768971:web:b1c5bcf73b63e0c268d90e",
+    measurementId: "G-Q4W3YX3RGB"
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'react';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './redux/reducers';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// redux를 사용하기 위해서
+// Provider로 App을 감싸주고
+// 미들 웨어를 저장해준다.
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
